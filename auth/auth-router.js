77060370a923c5e70eb00ab979a7cf6d43c28d8f
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-11-15 12:22:34 -0500

    Create route for the registration

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,33 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Users = require("../models/users-model");
+const { validateUser } = require("../models/users-helpers");
+const generateToken = require("./generateToken");
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const validateResult = validateUser(user);
+
+  if (validateResult.isSuccessful === true) {
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+     console.log(user)
+    Users.add(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  } else {
+    res.status(400).json({
+      message: "Invalid information about the user, see errors for details",
+      errors: validateResult.errors
+    });
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
-});
+router.post("/login", (req, res) => {
 
-module.exports = router;
+});
